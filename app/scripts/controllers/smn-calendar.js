9d74ea9c758028f7e3e9ee783f2ebc8539a55e3f
moonsemina <semina.moon@gmail.com>
2016-04-05 00:55:16 +0900

    directive link 에서 function 작업. 디자인 일부 수정 일정추가 modal 작업 중

--- app/scripts/controllers/smn-calendar.js
+++ app/scripts/controllers/smn-calendar.js
@@ -8,110 +8,145 @@
  * Controller of the semCalendarApp
  */
 angular.module('semCalendarApp')
-    .controller('SemCalendarCtrl', function ($scope, $q) {
-        $scope.weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        var getDates = function (year, month) {
-            var startDate = moment([year, month]);
-            var endDate = moment(startDate).endOf('month');
-            var blank;
-            switch (startDate.format('ddd')) {
-                case 'Mon' :
-                    blank = 1;
-                    break;
-                case 'Tue' :
-                    blank = 2;
-                    break;
-                case 'Wed' :
-                    blank = 3;
-                    break;
-                case 'Thu' :
-                    blank = 4;
-                    break;
-                case 'Fri' :
-                    blank = 5;
-                    break;
-                case 'Set' :
-                    blank = 6;
-                    break;
-                default    :
-                    blank = 0;
-                    break;
-            }
-            // insert blank
-            var _blankPromise = function (blank) {
-                return $q(function (resolve) {
-                    var dates = [];
-                    if (blank > 0) {
-                        for (var i = 0; i < blank; i++) {
-                            dates.push({id: '', num: ''});
-                        }
-                        resolve(dates);
-                    } else {
-                        resolve(dates);
-                    }
-                });
-            };
+    .controller('SemCalendarCtrl', function ($scope, $q, $timeout) {
 
-            // blank + dates
-            _blankPromise(blank).then(
-                function (dates) {
-                    $scope.dates = dates;
-                    for (var i = 0; i < endDate.format('D'); i++) {
-                        var d = moment(startDate).add(i, 'day');
-                        $scope.dates.push({id: d.format('YYYYMMDD'), num: i + 1});
-                    }
-                    if ($scope.dates.length % 7 !== 0) {
-                        for (var a = 0; a < ($scope.dates.length % 7); a++) {
-                            $scope.dates.push({id: '', num: ''});
-                        }
-                    }
-                }
-            );
-        };
-
-        $scope.prevMonth = function () {
-            if ($scope.month === 0) {
-                $scope.month = 11;
-                $scope.year--;
-            } else {
-                $scope.month--;
-            }
-            getDates($scope.year, $scope.month);
-        };
-
-        $scope.nextMonth = function () {
-            if ($scope.month === 11) {
-                $scope.month = 0;
-                $scope.year++;
-            } else {
-                $scope.month++;
-            }
-            getDates($scope.year, $scope.month);
-        };
-
-        $scope.getToday = function () {
-            $scope.year = moment().year();
-            $scope.month = moment().month();
-            $scope.today = moment().format('YYYYMMDD');
-            getDates($scope.year, $scope.month);
-        };
-
-        $scope.getToday();
-
-        $scope.blankCheck = function (day) {
-            if (day.id) {
-                $('.' + day.id).addClass('date-hover');
-            }
-        };
 
     })
 
-    .directive('basicCalendar', function () {
+    .directive('basicCalendar', function ($q) {
         return {
             restrict: 'EA',
             transclude: true,
-            templateUrl: '../views/basic-calendar.html'
+            scope: {
 
+            },
+            templateUrl: '../views/basic-calendar.html',
+            link: function (scope, elem, attr, ctrl, $transclude ) {
+                // Transclude ? header : ''
+                $transclude(function(clone){
+                    if(!clone.length){
+                        $('.smn-calendar .header').hide();
+                    }else{
+                        if(attr.headerbg ) scope.headerbg = attr.headerbg;
+                    }
+                });
+
+                // Calendar Setting
+                if(attr.width && attr.width > 199) $('.smn-calendar').css({ width: attr.width + 'px' });
+                if(attr.headerbg) $('.smn-calendar > .header').css({ 'background-color' : attr.headerbg });
+
+                // Week Init
+                scope.weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                var getDates = function (year, month) {
+                    var startDate = moment([year, month]);
+                    var endDate = moment(startDate).endOf('month');
+                    var blank;
+                    switch (startDate.format('ddd')) {
+                        case 'Mon' :
+                            blank = 1;
+                            break;
+                        case 'Tue' :
+                            blank = 2;
+                            break;
+                        case 'Wed' :
+                            blank = 3;
+                            break;
+                        case 'Thu' :
+                            blank = 4;
+                            break;
+                        case 'Fri' :
+                            blank = 5;
+                            break;
+                        case 'Set' :
+                            blank = 6;
+                            break;
+                        default    :
+                            blank = 0;
+                            break;
+                    }
+                    // insert blank
+                    var _blankPromise = function (blank) {
+                        return $q(function (resolve) {
+                            var dates = [];
+                            if (blank > 0) {
+                                for (var i = 0; i < blank; i++) {
+                                    dates.push({id:'', num:''});
+                                }
+                                resolve(dates);
+                            } else {
+                                resolve(dates);
+                            }
+                        });
+                    };
+
+                    // blank + dates
+                    _blankPromise(blank).then(
+                        function (dates) {
+                            scope.dates = dates;
+                            for (var i = 0; i < endDate.format('D'); i++) {
+                                var d = moment(startDate).add(i, 'day');
+                                scope.dates.push({id: d.format('dddYYYYMMDD'), num: i + 1});
+                            }
+                            if (scope.dates.length % 7 !== 0) {
+                                for (var a = 0; a < (scope.dates.length % 7); a++) {
+                                    scope.dates.push({id: '', num: ''});
+                                }
+                            }
+                        }
+                    );
+                };
+
+                // GET Today
+                scope.getToday = function () {
+                    scope.year = moment().year();
+                    scope.month = moment().month();
+                    scope.today = moment().format('YYYYMMDD');
+                    getDates(scope.year, scope.month);
+                };
+                scope.getToday();
+
+                // Prev Month
+                scope.prevMonth = function () {
+                    if (scope.month === 0) {
+                        scope.month = 11;
+                        scope.year--;
+                    } else {
+                        scope.month--;
+                    }
+                    getDates(scope.year, scope.month);
+                };
+
+                // Next Month
+                scope.nextMonth = function () {
+                    if (scope.month === 11) {
+                        scope.month = 0;
+                        scope.year++;
+                    } else {
+                        scope.month++;
+                    }
+                    getDates(scope.year, scope.month);
+                };
+
+                // Double Click Event
+                scope.doubleClick = function (day) {
+                    if (day.id) {
+                        scope.openModal(day);
+                    }
+                };
+
+
+                /**
+                 * Func Modal
+                 */
+                scope.openModal = function(day){
+                    $('.smn-modal').fadeIn(200);
+                };
+
+                scope.closeModal = function(){
+                    $('.smn-modal').fadeOut(200);
+                };
+
+            }
         };
     });
 
